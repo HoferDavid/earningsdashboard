7703bfbbbc1eb6cde650b1e6840ed18cfb198aef
David Hofer <chassis.poppets0x@icloud.com>
2024-11-09 18:08:38 +0100

    feat: add overview search field

--- frontend/src/app/components/pages/overview/overview.component.ts
+++ frontend/src/app/components/pages/overview/overview.component.ts
@@ -1,24 +1,59 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
-import { OverviewHeaderComponent } from "./overview-header/overview-header.component";
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest, startWith } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { BasicWidgetComponent } from '../../widgets/basic-widget/basic-widget.component';
 import { BasicWidget } from '../../../interfaces/basic-widget';
 import { FirestoreService } from '../../../services/firestore.service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-overview',
   standalone: true,
-  imports: [ MatButtonModule, MatIconModule, MatMenuModule, OverviewHeaderComponent, BasicWidgetComponent, CommonModule ],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    BasicWidgetComponent,
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './overview.component.html',
-  styleUrl: './overview.component.scss',
+  styleUrls: ['./overview.component.scss'],
 })
-export class OverviewComponent {
-
+export class OverviewComponent implements OnInit {
   firestoreService = inject(FirestoreService);
   
+  // Observable für alle Aktien (ungefiltert)
   stocks$: Observable<BasicWidget[]> = this.firestoreService.getStocks();
+
+  // FormControl für das Suchfeld
+  searchControl = new FormControl<string>('');
+
+  // Gefilterte Aktienliste basierend auf Suchtext
+  filteredStocks$: Observable<BasicWidget[]> = combineLatest([
+    this.stocks$,
+    this.searchControl.valueChanges.pipe(startWith(''))
+  ]).pipe(
+    map(([stocks, searchText]) => 
+      stocks.filter(stock => 
+        stock.name.toLowerCase().includes(searchText!.toLowerCase()) || 
+        stock.ticker.toLowerCase().includes(searchText!.toLowerCase())
+      )
+    )
+  );
+
+  ngOnInit() {
+    // Falls zusätzliche Initialisierung nötig ist
+  }
+
+  // TrackBy-Funktion für Performance
+  trackByTicker(index: number, stock: BasicWidget) {
+    return stock.ticker;
+  }
 }
--- frontend/src/app/components/widgets/basic-widget/basic-widget.component.ts
+++ frontend/src/app/components/widgets/basic-widget/basic-widget.component.ts
@@ -3,14 +3,13 @@ import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { BillionFormatPipe } from '../../../pipes/billion-format.pipe';
 import { QuarterFormatPipe } from '../../../pipes/quarter-format.pipe';
-import { MatIcon } from '@angular/material/icon';
 import { WidgetComponent } from '../../widget/widget.component';
 import { BasicWidget } from '../../../interfaces/basic-widget';
 
 @Component({
   selector: 'app-basic-widget',
   standalone: true,
-  imports: [MatIcon, CommonModule, BillionFormatPipe, QuarterFormatPipe, WidgetComponent],
+  imports: [CommonModule, BillionFormatPipe, QuarterFormatPipe, WidgetComponent],
   templateUrl: './basic-widget.component.html',
   styleUrl: './basic-widget.component.scss'
 })
