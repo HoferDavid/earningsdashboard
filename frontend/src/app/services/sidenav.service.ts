eba239eba09bb782c84f8bc66e764d2a25204a9b
David Hofer <chassis.poppets0x@icloud.com>
2024-11-11 16:35:10 +0100

    update: fix sidebar border when collapsed

--- frontend/src/app/services/sidenav.service.ts
+++ frontend/src/app/services/sidenav.service.ts
@@ -11,7 +11,7 @@ export class SidenavService {
   }
 
   get sidenavWidth(): string {
-    return this.collapsed() ? '64px' : '160px';
+    return this.collapsed() ? '0px' : '160px';
   }
 
   get profilPicWidth(): string {
