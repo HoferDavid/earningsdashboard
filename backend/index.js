5475b35244a5033b4ebdd98c1e92f7d6c12ba675
David Hofer <chassis.poppets0x@icloud.com>
2024-11-13 11:18:08 +0100

    update: add more data queries to backend

--- backend/index.js
+++ backend/index.js
@@ -6,28 +6,35 @@ const cron = require('node-cron');
 const serviceAccount = require('./serviceAccountKey.json');
 const stocks = require('./stocks.json');
 
+
 require('dotenv').config();
+
 
 // Firebase initialize
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
+
 const firestore = admin.firestore();
 const app = express();
 app.use(bodyParser.json());
+
 
 // Google Spreadsheet ID and API Key
 const spreadsheetId = process.env.SPREADSHEET_ID;
 const apiKey = process.env.API_KEY;
 
+
 // Function, to load Data from Google Spreadsheet
-async function getStockOverviewData(sheetName, revenueRow, quarterRow) {
-    
+async function getStockOverviewData(sheetName, revenueRow, quarterRow, netIncomeRow, grossMarginRow) {
   const revenueRange = `${sheetName}!A${revenueRow}:AZ${revenueRow}`;
   const quarterRange = `${sheetName}!A${quarterRow}:AZ${quarterRow}`;
-  const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values:batchGet?ranges=${revenueRange}&ranges=${quarterRange}&key=${apiKey}`;
-  
+  const netIncomeRange = `${sheetName}!A${netIncomeRow}:AZ${netIncomeRow}`;
+  const grossMarginRange = `${sheetName}!A${grossMarginRow}:AZ${grossMarginRow}`;
+
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values:batchGet?ranges=${revenueRange}&ranges=${quarterRange}&ranges=${netIncomeRange}&ranges=${grossMarginRange}&key=${apiKey}`;
+
   try {
     const response = await axios.get(url);
     return response.data.valueRanges;
@@ -35,6 +42,7 @@ async function getStockOverviewData(sheetName, revenueRow, quarterRow) {
     console.error('Error while retrieving data:', error);
   }
 }
+
 
 // Load Google Spreadsheet Data into Firestore for all stocks
 async function syncSpreadsheetToFirestore() {
@@ -46,14 +54,22 @@ async function syncSpreadsheetToFirestore() {
       const sheetName = `$${stock.ticker}`; // Prefix the ticker with '$'
       const revenueRow = stock.revenueRow;
       const quarterRow = stock.quarterRow;
+      const netIncomeRow = stock.netIncomeRow;
+      const grossMarginRow = stock.grossMarginRow;
 
-      const stockData = await getStockOverviewData(sheetName, revenueRow, quarterRow);
+
+      console.log(`Synchronizing ${stockName} with ticker ${stock.ticker}, revenueRow: ${revenueRow}, quarterRow: ${quarterRow}, netIncomeRow: ${netIncomeRow}, grossMarginRow: ${grossMarginRow}`);
+
+
+      const stockData = await getStockOverviewData(sheetName, revenueRow, quarterRow, netIncomeRow, grossMarginRow);
       console.log(`Received data for ${stockName}:`, stockData);
 
       if (stockData) {
         // Save data in Firestore
         const revenueData = stockData[0].values[0];
         const quarterData = stockData[1].values[0];
+        const netIncomeData = stockData[2].values[0];
+        const grossMarginData = stockData[3].values[0];
 
         const stockRef = firestore.collection('stocks').doc(stock.ticker);
         await stockRef.set({
@@ -61,6 +77,8 @@ async function syncSpreadsheetToFirestore() {
           ticker: stock.ticker,
           revenue: revenueData,
           quarter: quarterData,
+          netIncome: netIncomeData,
+          grossMargin: grossMarginData,
           url: stock.url,  // Add this line to save the URL
           updatedAt: admin.firestore.FieldValue.serverTimestamp() // Set the timestamp
         });
@@ -78,14 +96,17 @@ async function syncSpreadsheetToFirestore() {
   }
 }
 
-// API-Route to manual Synchronisation
+
+// API-Route for manual Synchronisation
 app.post('/api/sync', async (req, res) => {
   await syncSpreadsheetToFirestore();
   res.send('Spreadsheet synchronized.');
 });
 
+
 // Cronjob every 24 hours
-cron.schedule('0 0 * * *', syncSpreadsheetToFirestore);
+// cron.schedule('0 0 * * *', syncSpreadsheetToFirestore);
+
 
 // Start Server
 const PORT = 5001;
