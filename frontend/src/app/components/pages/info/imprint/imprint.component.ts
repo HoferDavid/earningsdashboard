5b9d749db66c682aa62a796c71d3001f8b7b565b
David Hofer <chassis.poppets0x@icloud.com>
2024-11-21 12:28:56 +0100

    feat: add imprint basics

--- frontend/src/app/components/pages/info/imprint/imprint.component.ts
+++ frontend/src/app/components/pages/info/imprint/imprint.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PagesHeaderComponent } from "../../../common/pages-header/pages-header.component";
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, PagesHeaderComponent],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
+
+  pageTitle = 'Imprint';
+
+  mail = 'info@davidhofer.com';
+  phone = '+39 3408959151';
 
 }
