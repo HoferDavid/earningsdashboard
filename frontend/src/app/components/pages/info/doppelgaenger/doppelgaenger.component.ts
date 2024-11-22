82094c136da78c921ee2015dcc0fe4f64a435817
David Hofer <chassis.poppets0x@icloud.com>
2024-11-22 13:15:58 +0100

    feat: add doppelgaenger page

--- frontend/src/app/components/pages/info/doppelgaenger/doppelgaenger.component.ts
+++ frontend/src/app/components/pages/info/doppelgaenger/doppelgaenger.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { PagesHeaderComponent } from '../../../common/pages-header/pages-header.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-doppelgaenger',
   standalone: true,
-  imports: [],
+  imports: [PagesHeaderComponent, MatButtonModule],
   templateUrl: './doppelgaenger.component.html',
   styleUrl: './doppelgaenger.component.scss'
 })
 export class DoppelgaengerComponent {
+
+  pageTitle = 'Doppelgänger';
+  doppelgaengerUrl = 'https://www.doppelgaenger.io/';
+  applePodcastsUrl = 'https://podcasts.apple.com/de/podcast/doppelg%C3%A4nger-tech-talk/id1519234952';
+  spotifyUrl = 'https://open.spotify.com/show/3vBAacJIGDEb7hBHgdYlw3';
+  discordUrl = 'https://www.doppelgaenger.io/discord/';
 
 }
