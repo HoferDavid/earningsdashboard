ab0bb8e34fec6d7bb3a466327a68bfccc2836d1b
David Hofer <chassis.poppets0x@icloud.com>
2024-11-27 11:16:09 +0100

    feat: save favorite theme in local storage

--- frontend/src/app/services/theme.service.ts
+++ frontend/src/app/services/theme.service.ts
@@ -5,10 +5,28 @@ import { Injectable, signal } from '@angular/core';
 })
 export class ThemeService {
 
-  lightMode = signal(false);
+  lightMode = signal(this.loadThemePreference());
+
+  constructor() {
+    this.applyTheme(this.lightMode());
+  }
 
   toggleTheme() {
     this.lightMode.update(current => !current);
-    document.documentElement.classList.toggle('light', this.lightMode());
+    this.applyTheme(this.lightMode());
+    this.saveThemePreference(this.lightMode());
+  }
+
+  private saveThemePreference(isLightMode: boolean): void {
+    localStorage.setItem('lightMode', JSON.stringify(isLightMode));
+  }
+
+  private loadThemePreference(): boolean {
+    const savedPreference = localStorage.getItem('lightMode');
+    return savedPreference ? JSON.parse(savedPreference) : false;
+  }
+
+  private applyTheme(isLightMode: boolean): void {
+    document.documentElement.classList.toggle('light', isLightMode);
   }
 }
