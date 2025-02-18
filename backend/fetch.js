f0d587f0d079a7e33a18883e9668d13c82dacf17
David Hofer <chassis.poppets0x@icloud.com>
2025-02-18 18:09:52 -0300

    fix: add console logs

--- backend/fetch.js
+++ backend/fetch.js
@@ -1,6 +1,19 @@
 require('dotenv').config();
 const admin = require('firebase-admin');
-const serviceAccount = JSON.parse(process.env.FIREBASE_PRIVATE_KEY); // Secret als JSON
+
+if (!process.env.FIREBASE_PRIVATE_KEY) {
+  console.error('🔥 FIREBASE_PRIVATE_KEY is not set!');
+  process.exit(1);
+}
+
+let serviceAccount;
+try {
+  serviceAccount = JSON.parse(process.env.FIREBASE_PRIVATE_KEY); // Secret als JSON
+} catch (error) {
+  console.error('❌ Failed to parse FIREBASE_PRIVATE_KEY:', error);
+  process.exit(1);
+}
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
